eb1643bc420563dcea33cacc5cf5324cef1b598e
eli <eliezra236@gmail.com>
2024-08-11 13:14:25 +0300

    test timing + promising

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,23 @@
 import {Queue, LimitedScheduler} from './index.js';
 
-function mathAdd(a: number, b: number) {
+async function mathAdd(a: number, b: number) {
     return a + b;
 }
 
-const queue = new Queue(1000);
-
+const queue = new Queue(5000);
+const timeXStart = Date.now();
 const x = await queue.add(() => mathAdd(1, 2));
-console.log(x);
+const timeYStarts = Date.now();
+const y = await queue.add(() => mathAdd(3, 4));
+const z = await queue.add(() => mathAdd(5, 6));
+const end = Date.now();
+console.log('res', {
+    x,
+    y,
+    z,
+    timeXTook: end - timeXStart,
+    timeYTook: end - timeYStarts
+});
 // const y = queue.add(() => mathAdd(3, 4));
 // const z = queue.add(() => mathAdd(5, 6));
 
