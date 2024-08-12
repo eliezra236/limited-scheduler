bedd467849bd188056dbb9f5e9c493e8e847e5d4
eli <eliezra236@gmail.com>
2024-08-12 09:45:14 +0300

    added to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {LimitedScheduler} from './LimitedScheduler.js';
 export {Queue} from './Queue.js';
 export {PromiseWithStatus} from './PromiseWithStatus.js';
+export {LimitedSchedulerByDone} from './LimitedSchedulerByDone.js';
 
